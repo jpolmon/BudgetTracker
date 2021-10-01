41118cabfd7f5a6c1848f39dce9fc8870e56e01b
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-10-01 08:33:30 -0500

    heroku deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,10 +6,10 @@ const FILES_TO_CACHE = [
   '/assets/css/style.css',
   '/assets/js/index.js',
   '/assets/js/db.js',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',  
   '/assets/images/icons/icon-192x192.png',
-  '/assets/images/icons/icon-512x512.png'
+  '/assets/images/icons/icon-512x512.png',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'  
 ];
 
 const PRECACHE = 'precache-v1';
